69391887eb0f4b2191378ae200322cabd694c324
Andrew Worcester <Andrew Worcester>
2017-06-05 14:43:46 -0400

    Adds a blog post function, changes appearence of text based on input size, and used two inputs for the h1 tag

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const personForm = document.querySelector('#personForm')
+const talkingForm = document.querySelector('#talkingForm')
 
 function handleSubmit(ev) {
+    console.log('yo')
     ev.preventDefault()
+
     const f = ev.target
     const name = f.personName.value
-    document.querySelector('h1').textContent = name
+    const pClass = f.personClass.value
+    const combo = name + " " + pClass
+    document.querySelector('h1').textContent = combo
+}
+
+
+
+function handleTalk(ev) {
+    console.log('hi')
+    ev.preventDefault()
+    const g = ev.target
+    const enteredWords=g.blogEntry
+    const oldWords = document.getElementById("p1").innerHTML
+    const combo = oldWords + enteredWords.value
+    document.getElementById("p1").innerHTML = combo
+    if (enteredWords.value.length > 10){
+        document.getElementById("p1").style.fontFamily = 'Comic Sans MS'
+    } else {
+        document.getElementById("p1").style.fontFamily = 'times'
+    }
 }
 
 personForm.addEventListener('submit', handleSubmit)
+
+talkingForm.addEventListener('submit', handleTalk)
