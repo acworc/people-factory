8e24c0bb08d5f889a48aba5c9a4a7e55c6a7c48a
Andrew Worcester <worcesac@rose-hulman.edu>
2017-06-07 10:59:14 -0400

    wrapped everything in an object

--- index.js
+++ index.js
@@ -1,108 +1,69 @@
-const personForm = document.querySelector('#personForm')
-const talkingForm = document.querySelector('#talkingForm')
+const PeopleFactory = {
+  init: function(formSelector) {
+    const f = document.querySelector(formSelector)
+    f.addEventListener('submit', this.handleSubmit.bind(this))
+  },
 
-function renderColor(color) {
+  renderColor: function(color) {
     const colorDiv = document.createElement('div')
     colorDiv.style.backgroundColor = color
     colorDiv.style.width = '100px'
     colorDiv.style.height = '50px'
 
     return colorDiv
-}
+  },
 
-function renderListItem(fieldName, value) {
+  renderListItem: function(fieldName, value) {
     const li = document.createElement('li')
-    li.innerHTML = `${fieldName}: ${value}`
+    const dt = document.createElement('dt')
+    const dd = document.createElement('dd')
+    dt.textContent = fieldName
+    dd.innerHTML = value
+    li.appendChild(dt)
+    li.appendChild(dd)
     return li
-}
+  },
 
-function renderList(personData) {
-    const list = document.createElement('ul')
-    Object.keys(personData).map(function(fieldName) {
-        const li = renderListItem(fieldName, personData[fieldName])
-        list.appendChild(li)
+  renderList: function(personData) {
+    const list = document.createElement('dl')
+
+    // Loop over ['name', 'favoriteColor', 'age']
+    Object.keys(personData).map((fieldName) => {
+      const li = this.renderListItem(fieldName, personData[fieldName])
+      list.appendChild(li)
     })
+
     return list
-}
+  },
 
-
-function handleSubmit(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const details = document.querySelector('#details')
-//   const name = f.personName.value
-//   const favoriteColor = f.favoriteColor.value
-//   const age = f.age.value
-
-  const person = {
-      name: f.personName.value,
-      favoriteColor: renderColor(f.favoriteColor.value).outerHTML,
-      age: f.age.value,
-  }
-
-  details.appendChild(renderList(person))
-
-//   const colorDiv = `
-//     <div style="background-color: ${favoriteColor}; width: 100px; height: 50px;"></div>
-//   `
-
-//   const nameLi = document.createElement("LI")
-//   nameLi.innerHTML = "Name: " + name
-// //   nameLi.appendChild(document.createTextNode("Name: " + name));
-
-//   const colorDivLi = document.createElement("LI")
-//   colorDivLi.innerHTML = "Favorite Color: " + colorDiv
-// //   colorDivLi.appendChild(document.createTextNode("Favorite Color: " + colorDiv));
-
-//   const ageLi = document.createElement("LI")
-//   ageLi.innerHTML = "Age: " + age
-// //   ageLi.appendChild(document.createTextNode("Age: " + age))
-
-//   details.appendChild(nameLi)
-//   details.appendChild(colorDivLi)
-//   details.appendChild(ageLi)
-  //li = document.createElement("li");
-//   details.innerHTML = `
-//     <ul>
-//       <li>Name: ${name}</li>
-//       <li>Favorite Color: ${colorDiv}</li>
-//       <li>Age: ${age}</li>
-//     </ul>
-//   `
-
-  const pClass = f.personClass.value
-  const combo = name + " " + pClass
-  document.querySelector('h1').textContent = combo
-
-
-  
-  // details.innerHTML = `<em>${name}</em>`
-
-//   const em = document.createElement('em')
-//   em.textContent = name
-//   details.appendChild(em)
-
-
-  
-}
-
-
-
-function handleTalk(ev) {
-    console.log('hi')
+  handleSubmit: function(ev) {
     ev.preventDefault()
-    const g = ev.target
-    const enteredWords=g.blogEntry
-    const oldWords = document.getElementById("p1").innerHTML
-    const combo = oldWords + enteredWords.value
-    document.getElementById("p1").innerHTML = combo
-    if (enteredWords.value.length > 10){
-        document.getElementById("p1").style.fontFamily = 'Comic Sans MS'
-    } else {
-        document.getElementById("p1").style.fontFamily = 'times'
+    const f = ev.target
+    const details = document.querySelector('#details')
+
+    const person = {
+      name: f.personName.value,
+      favoriteColor: this.renderColor(f.favoriteColor.value).outerHTML,
+      age: f.age.value,
     }
+
+    details.appendChild(this.renderList(person))
+  },
+    handleTalk: function(ev) {
+        console.log('hi')
+        ev.preventDefault()
+        const g = ev.target
+        const enteredWords=g.blogEntry
+        const oldWords = document.getElementById("p1").innerHTML
+        const combo = oldWords + enteredWords.value
+        document.getElementById("p1").innerHTML = combo
+        if (enteredWords.value.length > 10){
+            document.getElementById("p1").style.fontFamily = 'Comic Sans MS'
+        } else {
+            document.getElementById("p1").style.fontFamily = 'times'
+        }
+    }
+
 }
 
-personForm.addEventListener('submit', handleSubmit)
-
-talkingForm.addEventListener('submit', handleTalk)
+PeopleFactory.init('form#personForm')
